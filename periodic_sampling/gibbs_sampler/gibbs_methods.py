72abefca1d98e2dd1e80ce53d5948a8af5d41248
Kit Gallagher <gallagherkit@outlook.com>
2022-07-25 12:21:56 +0100

    Refactor gibbs sampling class (#13)

--- periodic_sampling/gibbs_sampler/gibbs_methods.py
+++ periodic_sampling/gibbs_sampler/gibbs_methods.py
@@ -7,26 +7,60 @@ import pandas as pd
 
 from gibbs_param import Parameter
 
-def gibbs_sampler(N, params, sample_period = 1, sample_burnin = 0):
-    """Conducts N iterations of a Gibbs Sampler.
-    
-    params: Dictionary of all parameters + constants required for 
-        calculating conditional posterior distributions. Parameters 
-        that will be updated should be instances of the Parameter class.
-    sample_period: How frequently to record samples - as successive
-        samples have some degree of correlation (forming a Markov Chain)
-    sample_burnin: Interations before recording, to allow the stationary
-        distribution to be reached
-    """
-    history = []
-    for n in range(N):
-        row = {}
-        random.shuffle(list(params.keys()))
-        for key in list(params.keys()):
-            if isinstance(params[key], Parameter):
-                value = params[key].sample(params)
-                params[key].value = value
-                row[key] = value
-        if ((N >= sample_burnin) & (N % sample_period == 0)):
-            history.append(row)
-    return pd.DataFrame(history)
+class GibbsSampler:
+    """Sampling class using Gibbs methods"""
+
+    def __init__(self, params):
+        """Constructor object, takes dictionary of parameters
+        
+        Parameters
+        ----------
+        params : Dict
+            Dictionary of all parameters + constants required for 
+            calculating conditional posterior distributions. Parameters 
+            that will be updated should be instances of the Parameter class.
+        """
+        self.params = params
+
+    def single_sample(self, param_name):
+        """Runs single sample of a parameter, updating the value 
+        inplace in the params dictionary and returning the updated value
+        for recording.
+        
+        Parameters
+        ----------
+        param_name : str
+            Key from params dictionary corresponding to Parameter
+            instance to sample from.
+        """
+        assert isinstance(self.params[param_name], Parameter), \
+            "Parameter name supplied must correspond to Parameter instance"
+        value = self.params[param_name].sample(self.params)
+        self.params[param_name].value = value
+        return value
+
+    def sampling_routine(self, N, sample_period = 1, sample_burnin = 0):
+        """Conducts N iterations of a Gibbs Sampler.
+        
+        Parameters
+        ----------
+        N : int
+            Number of iterations to sample over
+        sample_period : int
+            How frequently to record samples - as successive
+            samples have some degree of correlation (forming a Markov Chain)
+        sample_burnin : int
+            Interations before recording, to allow the stationary
+            distribution to be reached
+        """
+        params = self.params
+        history = []
+        for n in range(N):
+            row = {}
+            random.shuffle(list(params.keys()))
+            for key in list(params.keys()):
+                if isinstance(params[key], Parameter):
+                    row[key] = self.single_sample(key)
+            if ((n >= sample_burnin) & (n % sample_period == 0)):
+                history.append(row)
+        return pd.DataFrame(history)
