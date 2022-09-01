8ba3ee5ec9d09a62cc2c03c52b1c659281ea9bfd
Kit Gallagher <gallagherkit@outlook.com>
2022-09-01 10:16:43 +0100

    multistep sampling

--- periodic_sampling/inference_workflow.py
+++ periodic_sampling/inference_workflow.py
@@ -136,8 +136,8 @@ start_date = '01/01/2020'; bias_method = 'scale'
 bias = [0.5, 1.4, 1.2, 1.1, 1.1, 1.1, 0.6]  # Always given with monday first
 R0_list = ([1.0 + R0_diff] * int(time_steps/2)) + ([1.0 - R0_diff] * int(time_steps/2))
 
-seeds = list(range(4)); output = pd.DataFrame()
-step_num = 20
+seeds = list(range(6)); output = pd.DataFrame()
+step_num = 300
 
 for seed in seeds:
     np.random.seed(41)
@@ -145,8 +145,7 @@ for seed in seeds:
     model.simulate(T=time_steps, N_0=N_0, R_0=R0_list)
 
     # Report unbiased and biased data
-    rep = Reporter(model.case_data, start_date=start_date)  # Start on Mon 6th for ease
-
+    rep = Reporter(model.case_data, start_date=start_date)  
     truth_df = rep.unbiased_report()
     bias_df = rep.fixed_bias_report(bias=bias, method=bias_method)
 
@@ -169,16 +168,63 @@ for seed in seeds:
     for i in range(7):  # Weekday bias parameters
         params[("bias_" + str(i))] = bias_parameter(value=2 * np.random.random(), index=i)
 
-    for i in range(0, len(I_data)):  # Reproductive number values
-        params[("R_" + str(i))] = rt_parameter(value=2 * np.random.random(), index=i)
+    params["R_t"] = 1
 
 
     sampler = MixedSampler(params=params)
-    output = pd.concat([output, sampler.sampling_routine(step_num=step_num,
+    output1 = pd.concat([output1, sampler.sampling_routine(step_num=step_num,
                                                          sample_burnin=0,
                                                          chain_num=seed)],
                         axis=0)
     
 
-filename = f"synth_inference_T_{bias_method}_{time_steps}_N0_{N_0}_R0diff_{R0_diff}_It_{step_num}_seeds_{seed+1}.csv"
-output.to_csv('data/outputs/stepped_R/multichain/' + filename)
+filename = f"step1_inference_T_{bias_method}_{time_steps}_N0_{N_0}_R0diff_{R0_diff}_It_{step_num}_seeds_{seed+1}.csv"
+output1.to_csv('data/outputs/stepped_R/multichain/fixed_r_stage/' + filename)
+
+seeds = list(range(6)); output = pd.DataFrame()
+step_num = 300
+
+input = output1.mean()
+for seed in seeds:
+    np.random.seed(41)
+    model = RenewalModel()
+    model.simulate(T=time_steps, N_0=N_0, R_0=R0_list)
+
+    # Report unbiased and biased data
+    rep = Reporter(model.case_data, start_date=start_date) 
+
+    truth_df = rep.unbiased_report()
+    bias_df = rep.fixed_bias_report(bias=bias, method=bias_method)
+
+    np.random.seed(seed)
+    I_data = list(bias_df['Confirmed'])
+
+    params = {'bias_prior_alpha': 1, 'bias_prior_beta': 1,
+            'rt_prior_alpha': 1, 'rt_prior_beta': 1}  # Gamma dist
+
+    params['serial_interval'] = RenewalModel(R0=None).serial_interval
+    params['Rt_window'] = 7  # Assume it is constant for 7 days
+
+    for i, val in enumerate(I_data):  # Observed cases - not a Parameter
+        params[("data_" + str(i))] = val
+
+    data_initial_guess = sum(I_data)/len(I_data)  # Constant initial value
+    for i in range(0, len(I_data)):  # Ground truth data
+        params[("truth_" + str(i))] = truth_parameter(input[("truth_" + str(i))], index=i)  # might fix next time
+
+    for i in range(7):  # Weekday bias parameters
+        params[("bias_" + str(i))] = bias_parameter(input[("truth_" + str(i))], index=i)
+
+    for i in range(0, len(I_data)):  # Reproductive number values
+        params[("R_" + str(i))] = rt_parameter(value=2 * np.random.random(), index=i)
+
+
+    sampler = MixedSampler(params=params)
+    output2 = pd.concat([output2, sampler.sampling_routine(step_num=step_num,
+                                                         sample_burnin=0,
+                                                         chain_num=seed)],
+                        axis=0)
+    
+
+filename = f"step2_inference_T_{bias_method}_{time_steps}_N0_{N_0}_R0diff_{R0_diff}_It_{step_num}_seeds_{seed+1}.csv"
+output2.to_csv('data/outputs/stepped_R/multichain/fixed_r_stage/' + filename)
