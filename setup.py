f836a0f74643c623dcaeb960864366f515c28b56
Kit Gallagher <gallagherkit@outlook.com>
2022-09-13 14:44:00 +0100

    Package code (#29)

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='periodic_sampling',
+   version='1.0',
+   license="MIT",
+   description='Bayesian sampling methods to explore periodic data trends in epidemiological data',
+   author='Kit Gallagher',
+   packages=['periodic_sampling'], 
+   install_requires=['numpy', 'pandas', 'scipy'],
+   packages=find_packages(include=('periodic_sampling')),
+)
